f918e9a17361743fa13cfd7c58ca841002405266
SuperNever <1021165525@qq.com>
2017-11-01 23:46:01 +0800

    init

---
+++ examples/sn-btn.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="demo">
+      <Demo></Demo>
+      <i class="icon iconfont icon-check"></i>
+      <Demo type="success"></Demo>
+      <Demo type="info"></Demo>
+      <Demo type="warning"></Demo>
+      <Demo type="danger"></Demo>
+  </div>
+</template>
+
+<script>
+  import Demo from '../src/sn-btn/sn-btn';
+  export default {
+    name: 'app',
+    components: {
+      Demo
+    }
+  };
+</script>
+<style lang="less">
+    @import url(../static/styles/sn-btn.less);
+    .demo {
+        margin: 30px auto 0;
+        width: 500px;
+    }
+</style>
---
+++ src/sn-btn/sn-btn.vue
@@ -0,0 +1,23 @@
+<template>
+    <button class="sn-btn"
+      type="button"
+      value="点击我"
+      :class="[
+        type ? 'sn-btn--' + type : ''
+      ]"
+    >sdsdsd</button>
+</template>
+
+<script>
+  export default {
+    name: 'app',
+    props: {
+      type: {
+        type: String,
+        default: 'primary'
+      }
+    },
+    methods: {
+    }
+  };
+</script>
